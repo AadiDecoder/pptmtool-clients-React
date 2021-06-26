3126808cab299b958fac06d35d051ebf30143171
AdarshSrivastava <adarshsri1111@gmail.com>
2021-06-27 02:09:12 +0530

    Till Update Page

---
+++ src/components/Dashboard.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
+class Dashboard extends Component {
+  //lifecycle hook
+  //when component load , this gets execute
+  componentDidMount() {
+    this.props.getProjects();
+  }
+  render() {
+    const { projects } = this.props.project;
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+
+              <br />
+              <hr />
+              {projects.map((proj) => (
+                <ProjectItem key={proj.id} project={proj} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  project: state.project,
+});
+
+Dashboard.propTypes = {
+  projects: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+export default connect(mapStateToProps, { getProjects })(Dashboard);
---
+++ src/components/Project/updateProject.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class updateProject extends Component {
+  render() {
+    return <h1>Update Project</h1>;
+  }
+}
+
+export default updateProject;
